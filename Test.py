fd4750a9565e5cb3c0ab958de299fdde446a76e9
rahul t <rahult@rahuls-MacBook-Air.local>
2026-06-06 22:45:31 +0530

    Prompts for AI API

--- Test.py
+++ Test.py
@@ -1,14 +1,20 @@
 from dotenv import load_dotenv
 import os
-
-load_dotenv()
-
-api_key = os.getenv("OPEN_AI_API_KEY")
-
-
+from colorama import Fore, Style, init
 
 def main():
-    print("Hello")
+    print()
+    print("="* 40,end="\n\n")
+    print(Fore.GREEN + f"Prompt Tokens: 40")
+
+    print(Fore.GREEN + 
+        f"Completion Tokens: 50")
+
+    print(Fore.GREEN + 
+        f"Total Tokens: 60")
+    print(Style.RESET_ALL)
+    print("="*40)
 
 if __name__ == "__main__":
+    init()
     main()
